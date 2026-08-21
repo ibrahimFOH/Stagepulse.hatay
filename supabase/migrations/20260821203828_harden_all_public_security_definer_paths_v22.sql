61b6eb274638a6fcd3ef81787059cc5f981a105e
ibrahimFOH <ibrahimkavasoglu7@gmail.com>
2026-08-21 23:45:04 +0300

    sync(migrations): record production v22 in canonical filename

---
+++ supabase/migrations/20260821203828_harden_all_public_security_definer_paths_v22.sql
@@ -0,0 +1,7 @@
+DO $$
+DECLARE r record;
+BEGIN
+  FOR r IN SELECT p.oid,n.nspname,p.proname,pg_get_function_identity_arguments(p.oid) AS args FROM pg_proc p JOIN pg_namespace n ON n.oid=p.pronamespace WHERE n.nspname='public' AND p.prosecdef=true LOOP
+    EXECUTE format('ALTER FUNCTION %I.%I(%s) SET search_path = ''''',r.nspname,r.proname,r.args);
+  END LOOP;
+END $$;
