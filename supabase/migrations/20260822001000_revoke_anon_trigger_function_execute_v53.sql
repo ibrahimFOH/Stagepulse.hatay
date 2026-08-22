24020e0957f6d83c7b7f0b649cd7302ca0731a04
ibrahimFOH <ibrahimkavasoglu7@gmail.com>
2026-08-22 03:26:32 +0300

    chore(migrations): align trigger ACL migration version with production

--- supabase/migrations/20260822001000_revoke_anon_trigger_function_execute_v53.sql
+++
@@ -1,6 +0,0 @@
-REVOKE EXECUTE ON FUNCTION public.set_quote_defaults() FROM anon;
-REVOKE EXECUTE ON FUNCTION public.set_updated_at() FROM anon;
-REVOKE EXECUTE ON FUNCTION public.sync_offer_validity_compat() FROM anon;
-REVOKE EXECUTE ON FUNCTION public.set_quote_defaults() FROM PUBLIC;
-REVOKE EXECUTE ON FUNCTION public.set_updated_at() FROM PUBLIC;
-REVOKE EXECUTE ON FUNCTION public.sync_offer_validity_compat() FROM PUBLIC;
