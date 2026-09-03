de937b45b315992b681bdb3e0f3e1da027ea9c7d
Stagepulse.hatay <ibrahimkavasoglu7@gmail.com>
2026-09-03 15:04:07 +0300

    Bust Patron command-center assets after audit fixes

--- shared/runtime-config.js
+++ shared/runtime-config.js
@@ -10,7 +10,7 @@
     supabasePublishableKey: 'sb_publishable_yR_HlWlFbYYq22tQmiB9LA_acq6bQi6',
     siteAiUrl: null,
     fcm: {
-      apiKey: 'AIzaSyBZbLD2HpnrCDy4KJh9FUbwgBbI0m-jdeo',
+      apiKey: 'AIzaSyBZbLD2hpnrCDy4KJh9FUbwgBbI0m-jdeo',
       authDomain: 'stagepulse-905be.firebaseapp.com',
       projectId: 'stagepulse-905be',
       storageBucket: 'stagepulse-905be.firebasestorage.app',
@@ -33,6 +33,6 @@
   global.STAGEPULSE_RUNTIME = Object.freeze({supabaseUrl:url,supabasePublishableKey:key,siteAiUrl:siteAi,fcm:Object.freeze(fcm)});
   global.STAGEPULSE_FCM_CONFIG = Object.freeze(Object.assign({},fcm));
   if (global.location && /^\/admin\//.test(global.location.pathname)) {
-    [['link','stylesheet','admin-visual-overhaul.css?v=20260903-1'],['link','stylesheet','patron-center.css?v=20260903-3'],['script','', 'patron-center.js?v=20260903-3'],['link','stylesheet','owner-operating-system.css?v=20260903-3'],['script','', 'owner-operating-system.js?v=20260903-3'],['script','', 'command-center-bridge.js?v=20260903-2'],['script','', 'command-center-forms.js?v=20260903-1'],['script','', 'command-center-forms-router.js?v=20260903-1'],['script','', 'command-center-complete-modules.js?v=20260903-1']].forEach(function(x){var e=document.createElement(x[0]);if(x[1])e.rel=x[1];if(x[0]==='script')e.src=x[2];else e.href=x[2];e.defer=false;document.head.appendChild(e);});
+    [['link','stylesheet','admin-visual-overhaul.css?v=20260903-1'],['link','stylesheet','patron-center.css?v=20260903-3'],['script','', 'patron-center.js?v=20260903-3'],['link','stylesheet','owner-operating-system.css?v=20260903-3'],['script','', 'owner-operating-system.js?v=20260903-3'],['script','', 'command-center-bridge.js?v=20260903-3'],['script','', 'command-center-forms.js?v=20260903-2'],['script','', 'command-center-forms-router.js?v=20260903-2'],['script','', 'command-center-complete-modules.js?v=20260903-2']].forEach(function(x){var e=document.createElement(x[0]);if(x[1])e.rel=x[1];if(x[0]==='script')e.src=x[2];else e.href=x[2];e.defer=false;document.head.appendChild(e);});
   }
 })(typeof globalThis !== 'undefined' ? globalThis : window);
